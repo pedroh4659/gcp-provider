c78a468b5ee26139c887226adb0e2778218be2e5
pedro.martins <pedroh4659@gmail.com>
2022-04-19 14:35:46 -0300

    add application

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # Configuração básica do provedor GCP
 provider "google" {
 
-  credentials = file("tf-providing-4823385d0a0d.json")
+  credentials = file("kube-provider-0a51d5356468.json")
 
-  project = "tf-providing"
+  project = "kube-provider"
   region  = "southamerica-east1"
   zone    = "southamerica-east1-a"
 }
@@ -21,18 +21,18 @@ resource "google_compute_subnetwork" "subnet" {
     ip_cidr_range = "10.10.0.0/24"
 }
 
-# Criação de Cluster primário do GCP com 1 node
+# Criação de Cluster primário do GCP com 2 nodes
 resource "google_container_cluster" "primary" {
     name = "gke-teste"
-    location = "southamerica-east1"
-    remove_default_node_pool = true
+    location = "southamerica-east1-a"
     initial_node_count = 1
+    remove_default_node_pool = true
 }
 
-# Criação de node pool com 1 VM e2-small
+# Criação de node pool com 2 VM e2-small
 resource "google_container_node_pool" "primary_preemptible_nodes" {
     name       = "nodepool-teste"
-    location   = "southamerica-east1"
+    location   = "southamerica-east1-a"
     cluster    = google_container_cluster.primary.name
     node_count = 1
 
